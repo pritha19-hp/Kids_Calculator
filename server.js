472903c53f69de7d04ea97393cc569e76f467458
pritha19-hp <pritha123kundu@gmail.com>
2026-05-18 10:24:52 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const fs = require("fs-extra");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
